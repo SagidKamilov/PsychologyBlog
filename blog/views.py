b506dfcb2c81e85fbf82af7d1f3ae01a0624a944
SagidKamilov <89615854074a@gmail.com>
2024-01-09 23:02:07 +0300

    Добавил представления для приложения блог

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):
+    posts = Post.objects.all()
+
+    return render(request=request, template_name='blog/post_list.html', context={'posts': posts})
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+
+    return render(request=request, template_name='blog/post_detail.html', context={'post': post})
