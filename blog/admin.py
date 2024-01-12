308e5ee870a35c28d9f9fea8335a6a452bd04046
SagidKamilov <89615854074a@gmail.com>
2024-01-12 08:20:42 +0300

    Добавил комментарии к посту

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 
 @admin.register(Post)
@@ -7,3 +7,10 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'slug', 'author', 'status', 'dt_publish']
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['post', 'name', 'content', 'dt_created', 'active']
+    list_filter = ['active', 'dt_created', 'dt_updated']
+    search_fields = ['name', 'email', 'body']
--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,29 @@
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView, DetailView
 from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
 
 from psyhologyblog.settings import EMAIL_HOST_USER
-from .models import Post
-from .forms import EmailPostForm
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(klass=Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(request=request, template_name='blog/post_comment.html', context={'post': post, 'form': form,
+                                                                                    'comment': comment})
 
 
 def post_share(request, post_id):
-    post = get_object_or_404(klass=Post,
-                             id=post_id,
-                             status=Post.Status.PUBLISHED)
+    post = get_object_or_404(klass=Post, id=post_id, status=Post.Status.PUBLISHED)
 
     sent = False
 
@@ -40,17 +53,11 @@ class PostListView(ListView):
     template_name = 'blog/post_list.html'
 
 
-class PostDetailView(DetailView):
-    model = Post
-    queryset = Post.objects.filter(status=Post.Status.PUBLISHED)
-    context_object_name = 'post'
-    slug_url_kwarg = 'post'
-    template_name = 'blog/post_detail.html'
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, status=Post.Status.PUBLISHED, slug=post, dt_publish__year=year, dt_publish__month=month,
+                             dt_publish__day=day)
 
-    def get_object(self, queryset=None):
-        year = self.kwargs['year']
-        month = self.kwargs['month']
-        day = self.kwargs['day']
-        slug = self.kwargs['post']
-        return get_object_or_404(klass=self.queryset, dt_publish__year=year, dt_publish__month=month,
-                                 dt_publish__day=day, slug=slug)
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+
+    return render(request, template_name='blog/post_detail.html', context={'post': post, 'comments': comments, 'form': form})
