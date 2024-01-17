e373d220c955a1fe31ee17d43ee2d9ae64979641
SagidKamilov <89615854074a@gmail.com>
2024-01-17 22:04:58 +0300

    Добавил представление регистрации

--- registration/views.py
+++ registration/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from registration.forms import RegisterForm
 
-# Create your views here.
+
+def registration(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(to="/blog/posts")
+    else:
+        form = RegisterForm()
+
+    context = {'form': form}
+    return render(request=request, template_name='registration/register_page.html', context=context)
